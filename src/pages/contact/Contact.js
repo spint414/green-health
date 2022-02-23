dd75ebbf8337f58d33879e85e6a931cfe125102d
spint414 <nguyenvinhtrung1998@gmail.com>
2022-02-23 11:29:59 +0700

    them layer blue contact

--- src/pages/contact/Contact.js
+++ src/pages/contact/Contact.js
@@ -89,56 +89,58 @@ const Contact = () => {
       </div>
 
       <div className="form-send" style={{ backgroundImage: `url(${Banner5})` }}>
-        <div className="container">
-          <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1521.5196579332967!2d105.80363461667176!3d20.998928362060763!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3135aca2cd3fffff%3A0xd2d82eaa29579516!2zMzUgTMOqIFbEg24gVGhpw6ptLCBUaGFuaCBYdcOibiBUcnVuZywgVGhhbmggWHXDom4sIEjDoCBO4buZaSwgVmnhu4d0IE5hbQ!5e0!3m2!1svi!2s!4v1640171581846!5m2!1svi!2s"
-            allowfullscreen=""
-            loading="lazy"
-            style={{
-              border: "none",
-              width: "100%",
-              height: "100%",
-              // minHeight: 400,
-            }}
-            title="map"
-          ></iframe>
-          <div>
-            <h2>Gửi ý tưởng hoặc đề nghị của bạn tại đây</h2>
-            <Form>
-              <Form.Item
-                name="name"
-                rules={[
-                  { required: true, message: "Hãy điền đầy đủ họ và tên" },
-                ]}
-              >
-                <Input placeholder="Họ và tên" />
-              </Form.Item>
-              <Form.Item
-                name="phone"
-                rules={[
-                  { required: true, message: "Hãy điền đầy đủ số điện thoại" },
-                ]}
-              >
-                <Input placeholder="Số điện thoại" />
-              </Form.Item>
-              <Form.Item
-                name="email"
-                rules={[{ required: true, message: "Hãy điền đầy đủ email" }]}
-              >
-                <Input placeholder="Email" />
-              </Form.Item>
-              <Form.Item
-                name="content"
-                rules={[
-                  { required: true, message: "Hãy điền đầy đủ nội dung" },
-                ]}
-              >
-                <Input placeholder="Nội dung" />
-              </Form.Item>
-              <Form.Item htmlFor="submit">
-                <button>Gửi ngay</button>
-              </Form.Item>
-            </Form>
+        <div className="layer">
+          <div className="container">
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1521.5196579332967!2d105.80363461667176!3d20.998928362060763!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3135aca2cd3fffff%3A0xd2d82eaa29579516!2zMzUgTMOqIFbEg24gVGhpw6ptLCBUaGFuaCBYdcOibiBUcnVuZywgVGhhbmggWHXDom4sIEjDoCBO4buZaSwgVmnhu4d0IE5hbQ!5e0!3m2!1svi!2s!4v1640171581846!5m2!1svi!2s"
+              allowfullscreen=""
+              loading="lazy"
+              style={{
+                border: "none",
+                width: "100%",
+                height: "100%",
+                // minHeight: 400,
+              }}
+              title="map"
+            ></iframe>
+            <div>
+              <h2>Gửi ý tưởng hoặc đề nghị của bạn tại đây</h2>
+              <Form>
+                <Form.Item
+                  name="name"
+                  rules={[
+                    { required: true, message: "Hãy điền đầy đủ họ và tên" },
+                  ]}
+                >
+                  <Input placeholder="Họ và tên" />
+                </Form.Item>
+                <Form.Item
+                  name="phone"
+                  rules={[
+                    { required: true, message: "Hãy điền đầy đủ số điện thoại" },
+                  ]}
+                >
+                  <Input placeholder="Số điện thoại" />
+                </Form.Item>
+                <Form.Item
+                  name="email"
+                  rules={[{ required: true, message: "Hãy điền đầy đủ email" }]}
+                >
+                  <Input placeholder="Email" />
+                </Form.Item>
+                <Form.Item
+                  name="content"
+                  rules={[
+                    { required: true, message: "Hãy điền đầy đủ nội dung" },
+                  ]}
+                >
+                  <Input placeholder="Nội dung" />
+                </Form.Item>
+                <Form.Item htmlFor="submit">
+                  <button>Gửi ngay</button>
+                </Form.Item>
+              </Form>
+            </div>
           </div>
         </div>
       </div>
